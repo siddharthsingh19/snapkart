1659e91a0a104d2e6ad9951b085ebde327cc38e4
Siddharth Singh <siddharth.singh19@outlook.com>
2023-08-22 01:49:27 +0530

    image src changed. removed dev url from singleProduct

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -40,7 +40,7 @@ const SingleProduct = () => {
                     <div className="left">
                         <img
                             src={
-                                process.env.REACT_APP_DEV_URL +
+                                // process.env.REACT_APP_DEV_URL +
                                 product.img.data[0].attributes.url
                             }
                             alt=""
